adaef5be987d5c781d2c232af5322ee612442a47
Karol Misiarz <karol.misiarz@robovision.eu>
2021-05-03 11:41:51 +0200

    Remove reset callable from photo counter

--- hass_sleep_camera/photo_managers/photos_counter.py
+++ hass_sleep_camera/photo_managers/photos_counter.py
@@ -1,7 +1,6 @@
 class PhotosCounter:
-    def __init__(self, reset_callable):
+    def __init__(self):
         self._amount_of_photo_left = 0
-        self.reset_callable = reset_callable
 
     def update_photos_left(self, amount_of_photos_left):
         self._amount_of_photo_left = amount_of_photos_left
