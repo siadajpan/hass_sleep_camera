6d1132b78420cab0fd35a94b672d6865d2048092
Karol <siadajpan@o2.pl>
2021-08-16 19:24:44 +0200

    Remove queue, counters, slow photos and button

--- hass_sleep_camera/camera_controller.py
+++ hass_sleep_camera/camera_controller.py
@@ -1,12 +1,12 @@
+import cv2
 import logging
+import numpy as np
 import os
-from typing import Optional
 import picamera
 import picamera.array
 from picamera import PiCamera
-import cv2
-import numpy as np
 from singleton_decorator import singleton
+from typing import Optional
 
 from hass_sleep_camera.button.button_checker import ButtonChecker
 from hass_sleep_camera.photo_managers.photo_generator import PhotoGenerator
@@ -19,18 +19,19 @@ from hass_sleep_camera.settings import settings
 class CameraController:
     def __init__(self):
         self._camera = PiCamera()
-        self._camera.resolution = settings.Inputs.CAMERA_RESOLUTION
+        self._camera.resolution = settings.CAMERA_RESOLUTION
         self._log = logging.getLogger(self.__class__.__name__)
-        self._button_checker: Optional[ButtonChecker] = None
         self._photo_generator: Optional[PhotoGenerator] = None
-        self._quick_photos_running = False
-        self._photo_queue = PhotosQueue(
-            self._save_photo,
-            saving_frequency=settings.Timings.SLOW_PHOTOS_FREQUENCY,
-            queue_size_s=settings.Timings.QUEUE_SIZE_S
-        )
-        self._photo_queue.start()
-        self._photo_counter = PhotosCounter()
+
+    def start_photos(self):
+        self._start_photo_generator()
+
+    def stop_photos(self):
+        self._stop_photo_generator()
+
+    def stop(self):
+        self._log.info('Stopping camera controller')
+        self.stop_photos()
 
     def _start_photo_generator(self):
         self._log.debug('Starting photo generator')
@@ -38,7 +39,7 @@ class CameraController:
             self._log.debug('Photo generator has already been started')
             return
         self._photo_generator = PhotoGenerator(
-            settings.Timings.DELAY_BETWEEN_PHOTOS_S, self._make_photo)
+            settings.DELAY_BETWEEN_PHOTOS_S, self._make_photo)
         self._photo_generator.start()
 
     def _stop_photo_generator(self):
@@ -49,78 +50,26 @@ class CameraController:
             self._photo_generator.stop()
         self._photo_generator = None
 
-    def button_pressed_callable(self):
-        self._quick_photos_running = True
-        self._photo_queue.update_saving_frequency(
-            settings.Timings.QUICK_PHOTOS_FREQUENCY)
-        self._photo_counter.update_photos_left(
-            settings.Timings.AMOUNT_QUICK_PHOTOS,
-        )
-
-    def set_frequency_for_slow_photos(self):
-        self._quick_photos_running = False
-        self._photo_queue.update_saving_frequency(
-            settings.Timings.SLOW_PHOTOS_FREQUENCY
-        )
-
-    def _start_button_checker(self):
-        self._log.debug('Starting button checker')
-        if self._button_checker:
-            self._log.debug('Button checker has already been started')
-            return
-        self._button_checker = ButtonChecker(self.button_pressed_callable)
-        self._button_checker.start()
-
-    def _stop_button_checker(self):
-        self._log.debug('Stopping button checker')
-        if not self._button_checker:
-            self._log.debug('There was no button checker to stop')
-        else:
-            self._button_checker.stop()
-        self._button_checker = None
-
-    def start_photos(self):
-        self._start_photo_generator()
-        self._start_button_checker()
-
-    def stop_photos(self):
-        self._stop_photo_generator()
-        self._stop_button_checker()
-        self._photo_queue.flash_queue()
-
     def _make_photo(self):
         self._log.debug('Making photo')
         with picamera.array.PiRGBArray(self._camera) as stream:
             self._camera.capture(stream, format='rgb')
             image = stream.array
-            self._photo_queue.add_photo(image)
+            photo_name = datetime.fromtimestamp(photo_time). \
+                strftime("%Y_%m_%d__%H_%M_%S")
+            self._save_photo(photo_name, image)
 
     @staticmethod
     def _check_folders_exists():
         if not os.path.exists(settings.Folders.ROOT_FOLDER):
-            os.mkdir(settings.Folders.ROOT_FOLDER)
-        if not os.path.exists(settings.Folders.WAKE_UP_FOLDER):
-            os.mkdir(settings.Folders.WAKE_UP_FOLDER)
+            os.mkdir(settings.ROOT_FOLDER)
         if not os.path.exists(settings.Folders.SLEEP_FOLDER):
-            os.mkdir(settings.Folders.SLEEP_FOLDER)
+            os.mkdir(settings.SLEEP_FOLDER)
 
     def _save_photo(self, photo_name: str, photo: np.array):
         self._log.debug(f'Saving photo {photo_name}')
         self._check_folders_exists()
-        if self._quick_photos_running:
-            photo_path = os.path.join(settings.Folders.WAKE_UP_FOLDER,
-                                      photo_name) + '.jpg'
-            self._photo_counter.update_photo_counter()
-            if self._photo_counter.counter_done():
-                self.set_frequency_for_slow_photos()
-        else:
-            photo_path = os.path.join(settings.Folders.SLEEP_FOLDER,
-                                      photo_name) + '.jpg'
-
+        photo_path = os.path.join(settings.Folders.SLEEP_FOLDER,
+                                  photo_name) + '.jpg'
         self._log.debug(f'Writing image to file {photo_path}')
         cv2.imwrite(photo_path, photo)
-
-    def stop(self):
-        self._log.info('Stopping camera controller')
-        self.stop_photos()
-        self._photo_queue.stop()
--- hass_sleep_camera/settings/settings.py
+++ hass_sleep_camera/settings/settings.py
@@ -1,36 +1,17 @@
 import os
 
+DELAY_BETWEEN_PHOTOS_S = 1  # photo done every n second
+ROOT_FOLDER = '/home/pi/images'
+SLEEP_FOLDER = os.path.join(ROOT_FOLDER, 'sleep')
+CAMERA_RESOLUTION = (2592, 1944)
+
 
 class Mqtt:
-    ADDRESS = '192.168.0.164'
+    ADDRESS = '192.168.0.1'
     PORT = 1883
     USERNAME = 'karol'
     PASSWORD = 'klapeczki'
     TOPIC = 'camera/master_bedroom/bed/'
     ERROR_TOPIC = 'errors/camera/master_bedroom/bed/'
-
-
-class Messages:
     START_PHOTOS = Mqtt.TOPIC + "start_photos"
     STOP_PHOTOS = Mqtt.TOPIC + "stop_photos"
-
-
-class Timings:
-    DELAY_BETWEEN_PHOTOS_S = 10  # photo done every n second
-    QUICK_PHOTOS_FREQUENCY = 1  # every n-th photo will be saved
-    SLOW_PHOTOS_FREQUENCY = 10  # every n-th photo will be saved
-    AMOUNT_QUICK_PHOTOS = 60  # save n quick photos before going back to slow
-    # saved images are from n seconds back - how much time in seconds
-    # back before pressing button we want images to be saved
-    QUEUE_SIZE_S = 120
-
-
-class Folders:
-    ROOT_FOLDER = '/home/pi/images'
-    WAKE_UP_FOLDER = os.path.join(ROOT_FOLDER, 'wake_up')
-    SLEEP_FOLDER = os.path.join(ROOT_FOLDER, 'sleep')
-
-
-class Inputs:
-    BUTTON_NUMBER = 17
-    CAMERA_RESOLUTION = (2592, 1944)
